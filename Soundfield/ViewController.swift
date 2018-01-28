e521af478342d1d3a83da4a914d96a599ecda064
Zachary Stegall <zachstegall@gmail.com>
2018-01-27 17:57:37 -0800

    Animate welcome screen | Light status bar

--- Soundfield/ViewController.swift
+++ Soundfield/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var connectButton: UIButton!
+
+    var hasAnimatedUI: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        hideUI()
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        if !hasAnimatedUI {
+            showUI()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func hideUI() {
+        welcomeLabel.alpha = 0.0
+        instructionsLabel.alpha = 0.0
+        connectButton.alpha = 0.0
+    }
+
+    func showUI() {
+        hasAnimatedUI = true
+        let duration = 0.75
+        let delay = 0.5
+
+        func animateWelcome() {
+            UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions(rawValue: 0), animations: {
+                self.welcomeLabel.alpha = 1.0
+            }, completion: animateInstructions)
+        }
+
+        func animateInstructions(finished: Bool) {
+            UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions(rawValue: 0), animations: {
+                self.instructionsLabel.alpha = 1.0
+            }, completion: animateConnect)
+        }
+
+        func animateConnect(finished: Bool) {
+            UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions(rawValue: 0), animations: {
+                self.connectButton.alpha = 1.0
+            }, completion: nil)
+        }
+
+        animateWelcome()
     }
     
 
